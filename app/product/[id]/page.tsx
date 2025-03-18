c4687ed4d3fb468b27b387eb48fe940ee86d8ab6
Md Rehanul Haque <rehan78khan87@gmail.com>
2025-03-18 18:50:30 +0530

    3

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -48,10 +48,10 @@ export default async function page({
       <div className="mx-4 md:max-w-5xl md:mx-auto  flex flex-col md:flex-row gap-4 items-center justify-center md:justify-evenly mt-8 md:mt-16 bg-purple-400 p-4 rounded-xl shadow-md">
         <div>
           <Image
-            src={data?.productImage[0].url!}
+            src={data?.productImage[0].url! || ""}
             height={300}
             width={300}
-            alt={data?.title!}
+            alt={data?.title || ""}
           />
         </div>
         <div className="flex flex-col gap-4">
@@ -60,25 +60,25 @@ export default async function page({
             <Button>Get Best price</Button>
           </div>
           <div>
-            <h1 className="font-bold text-2xl my-2">{data?.title}</h1>
+            <h1 className="font-bold text-2xl my-2">{data?.title || ""}</h1>
             <div className="overflow-x-auto">
               <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <tbody>
                   <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                     <td className="px-6 py-4">Price</td>
-                    <td className="px-6 py-4">{data?.price}</td>
+                    <td className="px-6 py-4">{data?.price || ""}</td>
                   </tr>
                   <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                     <td className="px-6 py-4">Material</td>
-                    <td className="px-6 py-4">{data?.material}</td>
+                    <td className="px-6 py-4">{data?.material || ""}</td>
                   </tr>
                   <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                     <td className="px-6 py-4">Feature</td>
-                    <td className="px-6 py-4">{data?.feature}</td>
+                    <td className="px-6 py-4">{data?.feature || ""}</td>
                   </tr>
                   <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                     <td className="px-6 py-4">Description</td>
-                    <td className="px-6 py-4">{data?.description}</td>
+                    <td className="px-6 py-4">{data?.description || ""}</td>
                   </tr>
                 </tbody>
               </table>
@@ -100,7 +100,7 @@ export default async function page({
 
       <div className="mx-4 md:max-w-5xl md:mx-auto  mt-8 md:mt-16 bg-purple-400 p-4 rounded-xl shadow-md">
         <h1 className="font-bold text-2xl my-2 text-center text-gray-900">
-          Looking for {data?.title}?
+          Looking for {data?.title || ""}?
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4">
           <div>
