897fadd0781b0e1f4ebaacdb2a709287f8971457
Md Rehanul Haque <rehan78khan87@gmail.com>
2025-03-27 15:23:01 +0530

    22

---
+++ app/api/text/route.ts
@@ -0,0 +1,27 @@
+ /* eslint-disable  @typescript-eslint/no-explicit-any */
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(req: NextRequest) {
+  try {
+    return NextResponse.json({
+      message: "test", 
+      success: true,
+    }, { status: 201 });
+
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({
+      message: "Query Submission Failed", 
+      success: false
+    }, { status: 500 });
+  }
+}
+
+// Optional: Configure route settings
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+  maxDuration: 10 // Vercel serverless function timeout
+};
